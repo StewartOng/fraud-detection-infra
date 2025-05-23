3145c7f8bdefc3b0050cf44839ced9973bda0922
stewartong <ong.hueh.chuah@gmail.com>
2025-05-24 00:03:18 +0800

    Update on 23May2025 11.56pm

--- main.tf
+++ main.tf
@@ -30,6 +30,28 @@ resource "aws_api_gateway_method" "post" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.fraud_api.id
+  resource_id = aws_api_gateway_resource.check.id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = "200"
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.fraud_api.id
+  resource_id = aws_api_gateway_resource.check.id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = aws_api_gateway_rest_api.fraud_api.id
   resource_id             = aws_api_gateway_resource.check.id
